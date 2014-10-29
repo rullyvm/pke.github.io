5b14367c70d64f2cfc805037ca5aef9a134522ba
daniel <danielbporter@gmail.com>
2014-10-29 16:14:28 -0400

    one click subscribe javascript

--- js/subscribe.js
+++ js/subscribe.js
@@ -3,7 +3,7 @@ function SubscribeEmail(email) {
         'email':email,
 	'dont_redirect':'true'
     }   
-    $.ajax('//pledgedev.mayday.us/r/subscribe', {data:data, type:'POST'}).done(function() {
+    $.ajax('//pledge.mayday.us/r/subscribe', {data:data, type:'POST'}).done(function() {
         $('#result').html("<p> it worked! you're subscribed!");
     }).fail(function() {
         $('#result').html("<p> Oh no, there was a problem subscribing your email!");
