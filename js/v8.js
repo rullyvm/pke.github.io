8bdb9e4059aea2e4be25f02a8529a117bcff665d
Rio Flores <rio517@gmail.com>
2014-10-29 12:02:50 -0400

    Cleanup: some JS and CSS cleaning up

--- js/v8.js
+++ js/v8.js
@@ -19,27 +19,48 @@ limitations under the License.
 
 */
 
-function addCommas(nStr) {
-  nStr += '';
-  x = nStr.split('.');
-  x1 = x[0];
-  x2 = x.length > 1 ? '.' + x[1] : '';
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + ',' + '$2');
-  }
-  return x1 + x2;
+
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function updateTotals() {
   jQuery.getJSON('https://pledge.mayday.us/r/total', function(data) {
     totalRaised = data.totalCents/100 + (1408240.42+1775000);//+offline contributions
     totalRaised = Math.round(totalRaised);
-    $('#totalAmount').text('$' + addCommas(totalRaised));
+    $('#total-donated').text('$' + numberWithCommas(totalRaised));
   });
 
-  jQuery.getJSON('https://pledge.mayday.us/r/num_pledges',
-    function(data) {
-    $('#numPledges').text(addCommas(data.count+4917+6));//+offline contributions
+  jQuery.getJSON('https://pledge.mayday.us/r/num_pledges', function(data) {
+    $('#total-donors').text(numberWithCommas(data.count+4917+6));//+offline contributions
   });
 }
+// Normalize Carouselw5 Heights - pass in Bootstrap Carousel items.
+$.fn.carouselHeights = function() {
+    var items = $(this),
+        heights = [],
+        tallest;
+
+    var normalizeHeights = function() {
+        items.each(function() {
+            heights.push($(this).height());
+        });
+        tallest = Math.max.apply(null, heights);
+        items.each(function() {
+          console.log('tallest:',tallest)
+            $(this).css('min-height',tallest + 'px');
+        });
+    };
+
+    normalizeHeights();
+
+    $(window).on('resize orientationchange', function () {
+        tallest = 0;
+        heights.length = 0;
+
+        items.each(function() {
+          $(this).css('min-height','0');
+        });
+        normalizeHeights();
+    });
+};
