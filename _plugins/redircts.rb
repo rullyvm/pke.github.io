4da3ea9b49611c5d9f7fee93c48c6757cb328f82
Rio Flores <rio517@gmail.com>
2014-11-11 00:14:10 -0500

    Jekyll: adding redirects plugin
    Homegrown redirects plugin to add redirects to nginx installs.

---
+++ _plugins/redircts.rb
@@ -0,0 +1,39 @@
+module Jekyll
+
+
+  # Place holder - reference: https://gist.github.com/920651
+  class NginxRedirectsFile < StaticFile
+    def write(dest)
+      # do nothing
+    end
+  end
+
+  # generate nginx redirects file
+  class NginxRedirects < Generator
+
+    safe true
+    priority :low
+
+    # find all posts with a redirect property and create a new page for each entry
+    def generate(site)
+      pages_with_redirects = (site.pages + site.posts).select{|post| post.data.key? 'redirects' }
+      output = pages_with_redirects.map do |page|
+        page.data['redirects'].map do |redirect_origin|
+          nginx_redirect(redirect_origin, page.url)
+        end
+      end.flatten.uniq.join("\n")
+
+      if output.length > 0
+        File.open('_site/redirects.nginx', 'w') { |file| file.write(output) }
+        # Add this output file so it won't be "cleaned away"
+        site.static_files << NginxRedirectsFile.new(site, site.source, '', 'redirects.nginx')
+      end
+    end
+
+    def nginx_redirect(origin,destination)
+      "location #{origin} { return 301 #{destination}; }"
+    end
+
+  end
+
+end
