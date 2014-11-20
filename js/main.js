357c5e5e278e63cb6dae89d17d8ff2d752199e52
Rio Flores <rio517@gmail.com>
2014-11-20 14:14:41 -0500

    Revert "CLEANING: move JS to footer and code formating"
    This reverts commit 1daa78d569f7e9cbcd75ac47834c1d743f6de2c2.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/* Copyright 2014 Mayday PAC
+/*
+
+Copyright 2014 Mayday PAC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,30 +18,31 @@ limitations under the License.
 
 $(document).ready(function() {
 
+
 	//background scrolling effects
 
     var scrollorama = $.scrollorama({
         blocks:'.scrollblock'
     });
-
+    
     scrollorama
     	.animate('.intro-text',{delay:200, duration:300, property:'opacity', start:1, end:0})
     	.animate('.background',{delay:0, duration:200, property:'opacity', start:1, end:0.7});
-
-
+    	
+    
     //Draggable scroll boxes
-
+    	
     (function($) {
 	    $.fn.drags = function(opt) {
-
+	
 	        opt = $.extend({handle:"",cursor:"move"}, opt);
-
+	
 	        if(opt.handle === "") {
 	            var $el = this;
 	        } else {
 	            var $el = this.find(opt.handle);
 	        }
-
+	
 	        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
 	            if(opt.handle === "") {
 	                var $drag = $(this).addClass('draggable');
@@ -67,63 +70,63 @@ $(document).ready(function() {
 	                $(this).removeClass('active-handle').parent().removeClass('draggable');
 	            }
 	        });
-
+	
 	    }
 	})(jQuery);
-
+	
 	$('.drags').drags();
-
+	
 	//Functionality for closing out error boxes
-
+	
 	$( ".error-close" ).click(function() {
     var boxToClose = $(this).closest('li');
 	  boxToClose.fadeOut( 300 );
     setTimeout(function(){boxToClose.fadeIn( 300 );}, 3000);
 	});
-
+	
 	//Fixed 'pledge' bar
-
-	$(window).scroll(function() {
+	
+	$(window).scroll(function() {    
 	    var scroll = $(window).scrollTop();
-
+	
 	    if (scroll >= 1000) {
 	        $(".cta").addClass("fix");
 	    } else {
 	        $(".cta").removeClass("fix");
 	    }
 	});
-
+	
 	//Removing the animation background after scrolling
-
-	$(window).scroll(function() {
+	
+	$(window).scroll(function() {    
 	    var scroll = $(window).scrollTop();
-
+	
 	    if (scroll >= 500) {
 	        $(".background").removeClass("blink");
 	    } else {
 	        $(".background").addClass("blink");
 	    }
 	});
-
+	
 	//Tooltip content
-
+	
 	$('.tooltip.superpacs').tooltipster({
         content: $('<p>In 2010, two court rulings (<em>Citizens United v. Federal Election Commission</em> and <em>Speechnow.org v. FEC</em>) gave rise to a form of political action committee ("Super PACs") that can spend unlimited sums of money on election activities, and can accept donations from any source with no limits on donation size.</p>'),
         animation: 'grow',
         interactive: true,
         speed:200
     });
-
+    
     $('.tooltip.princeton').tooltipster({
         content: $('<p><a href="http://www.princeton.edu/~mgilens/Gilens%20homepage%20materials/Gilens%20and%20Page/Gilens%20and%20Page%202014-Testing%20Theories%203-7-14.pdf" class="button" target="_blank">View the report</a></p>'),
         animation: 'grow',
         interactive: true,
         speed:200
     });
-
-
+    
+    
     //Functionality for clickable vid gallery
-
+    
     $(".vid-link").on("click", function(event) {
 		event.preventDefault();
 		$(".big-vid .vid-contain iframe").prop("src", $(event.currentTarget).attr("href"));
@@ -133,11 +136,11 @@ $(document).ready(function() {
 		$( ".vid-link" ).removeClass( "active" );
 		$( this ).addClass( "active" );
 	});
-
-
-
+	
+	
+	
 	//Updating the thermometer with live data
-
+	
 	function setText(el, val) {
 	  el = document.getElementById(el);
 	  if (el.textContent !== undefined)
@@ -145,7 +148,7 @@ $(document).ready(function() {
 	  else
 	    el.innerText = val;
 	}
-
+    
 	function totalRaisedCB(totalRaisedCents) {
 	  var totalRaised = (totalRaisedCents / 100) - 1000000;
 	  var totalMillions = totalRaised / 1000000;
@@ -154,19 +157,19 @@ $(document).ready(function() {
 	  if (percent > 100) {
 	    percent = 100
 	  }
-	  document.getElementById("currentBar").style.width= Math.min(100, percent) + '%';
+	  document.getElementById("currentBar").style.width= Math.min(100, percent) + '%';  
 
 	var totalRaisedRounded = totalMillions.toFixed(1);
 	var complete_total_message = '$' + totalRaisedRounded + 'M';
 	setText("currentNumber", complete_total_message);
 
 	}
-
+	
 	jQuery.getJSON('https://pledge.mayday.us/r/total',
 	   function(data) {
 	     totalRaisedCB(data.totalCents);
 	   });
-
+	   
 	function ready(fn) {
 	  if (document.addEventListener) {
 	    document.addEventListener('DOMContentLoaded', fn);
@@ -177,36 +180,41 @@ $(document).ready(function() {
 	    });
 	  }
 	}
-
-
-
+	
+	
+	
 	//Setting the deadline countdown dynamically
-
+	
 	var deadline = new Date(Date.UTC(2014,06,05,05,00,00,00)).getTime();
 
 	var days;
 	var daysRounded;
-
+	
 	var days_display = document.getElementById("days_display");
-
+	
 	setInterval(function () {
-
+	
 	  // Calculate
 	  var current_date = new Date().getTime();
 	  var seconds_remaining = (deadline - current_date) / 1000;
-
+	
 	  days = parseInt(seconds_remaining / 86400);
 	  daysRounded = days + 1;
 	  seconds_remaining = seconds_remaining % 86400;
-
+	
 	  // Account for the deadline
 	  if (seconds_remaining <= 0) {
 	    days = '00';
 	  }
-
+	
 	  // Render
 	  setText("days_display", daysRounded);
-
+	
 	}, 10);
+	
 
+	
+    
+    
+    
 });
--- js/v8.js
+++ js/v8.js
@@ -35,36 +35,6 @@ function updateTotals() {
     $('#total-donors').text(numberWithCommas(data.count+3580));//+offline contributions
   });
 }
-
-function post_to_url(path, params, method) {
-  method = method || "POST"; // Set method to post by default, if not specified.
-
-  // The rest of this code assumes you are not using a library.
-  // It can be made less wordy if you use one.
-  var form = document.createElement("form");
-  form.setAttribute("method", method);
-  form.setAttribute("action", path);
-
-  for (var key in params) {
-    if (params.hasOwnProperty(key)) {
-      var hiddenField = document.createElement("input");
-      hiddenField.setAttribute("type", "hidden");
-      hiddenField.setAttribute("name", key);
-      hiddenField.setAttribute("value", params[key]);
-      form.appendChild(hiddenField);
-    }
-  }
-
-  document.body.appendChild(form);
-  form.submit();
-}
-
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
 // Normalize Carouselw5 Heights - pass in Bootstrap Carousel items.
 $.fn.carouselHeights = function() {
   var items = $(this),
